21af28a383b0ea07840c9247c8f9e11d92d5945c
delta <pchmiel93@gmail.com>
2020-10-20 00:47:38 +0200

    Added a proper DB Interface view w/ table list from sqlite_master

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ejsdemo
-Study in, or possibly later, a home for stray snippets for an ExpressJS server talking to a SQLite database
+Study in, or possibly later, a home for stray snippets for an ExpressJS server talking to a SQLite database.
+
+To-dos:
+- CSS cleanup to not require the <p> of grace
+- a sort of menu in Home
+- DB Interface:
+    - view/mixin for any given table
--- routing.js
+++ routing.js
@@ -8,12 +8,33 @@ function setupRouting(app, db) {
     }); 
 
     app.get('/db/', function(req, res, next) {
-        function renderRows(err, rows) {
-            console.log("Got rows: " + JSON.stringify(rows));           
-            res.render('db', { title: 'DB Interface', all_tables: rows });
+        var all_tables = [];
+        function processRows(err, row) {
+            query = row["sql"];
+            lparen = query.indexOf("(");
+            schemaArr = new Array;
+            schemaStr = query.slice(lparen+1, -1);
+            schemaStrArr = schemaStr.split(",");
+            console.log(" schemaStrArr is " + schemaStrArr);
+            for (col of schemaStrArr) {
+                colStmtArr = col.split(" ");
+                console.log(" col is " + col);
+                if (colStmtArr.length == 2) {
+                    colStmtObj = { name: colStmtArr[1], dataType: colStmtArr[0] };
+                } else {
+                    colStmtObj = { name: colStmtArr[0] };
+                }
+                schemaArr.push(colStmtObj);
+            }
+            all_tables.push({ name: row["name"], schema: schemaArr });
+            console.log("Table has query: " + query);
         }
-        console.log('We did a /db/!');
-        db.all("SELECT name FROM sqlite_master WHERE type='table'", renderRows);
+        function renderRows(err, rows) {
+            console.log("Finished! (" + rows + ")");
+            res.render('db', { title: 'DB Interface', all_tables: all_tables });
+        }
+        db.each("SELECT name, sql FROM sqlite_master WHERE type='table'",
+            processRows, renderRows);
 
     });
 }
