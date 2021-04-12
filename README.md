b3620eafd0121a9aa62617d747f2583678ecde65
delta <pchmiel93@gmail.com>
2021-04-12 23:11:17 +0200

    exposed schema fetching, parsing to other views

--- README.md
+++ README.md
@@ -3,11 +3,6 @@ Study in, or possibly later, a home for stray snippets for an ExpressJS server t
 
 To-dos:
 - DB Interface:
-    - Table index:
-        - move schema to table view
-            - isolate schema extraction
     - Table view:
-        - note row count
-        - get schema
         - list all column keys
         - SELECT args from GET args
--- routing.js
+++ routing.js
@@ -7,56 +7,64 @@ function setupRouting(app, db) {
         res.render('index', { title: 'Home' });
     }); 
 
+    // Parse a SQL schema statement and return array of columns
+    function getSchemaFromSql(sqlStr) {
+        // extract schema from sql syntax
+        const leftParenPos = sqlStr.indexOf("(");
+        const schemaStr = sqlStr.slice(leftParenPos+1, -1);
+        
+        // divide schema into columns
+        const columnStrs = schemaStr.split(",");
+        
+        var schemaArr = new Array;
+        for (icol of columnStrs) {
+            colStatementArr = icol.trim().split(/\s+/);
+            if (colStatementArr.length == 1) {
+                // type is not given
+                colStatementObj = { name: colStatementArr[0] };
+            } else {
+                // type is given
+                colStatementObj = { name: colStatementArr[0], dataType: colStatementArr[1] };
+            }
+            schemaArr.push(colStatementObj);
+        }
+        return schemaArr;
+    }
+
     // GET tables overview
     app.get('/table/', function(req, res, next) {
         var all_tables = [];
-
+        
         // Serialize given single row
-        function processRows(err, row) {
+        function processRow(err, row) {
+            console.log(`Got row: "${JSON.stringify(row)}..."`);
             queryStr = row["sql"];
-
-            // extract schema from sql syntax
-            leftParenPos = queryStr.indexOf("(");
-            schemaStr = queryStr.slice(leftParenPos+1, -1);
-            
-            // divide schema into columns
-            columnStrs = schemaStr.split(",");
-            
-            schemaArr = new Array;
-            for (icol of columnStrs) {
-                colStatementArr = icol.trim().split(/\s+/);
-                if (colStatementArr.length == 1) {
-                    // type is not given
-                    colStatementObj = { name: colStatementArr[0] };
-                } else {
-                    // type is given
-                    colStatementObj = { name: colStatementArr[0], dataType: colStatementArr[1] };
-                }
-                schemaArr.push(colStatementObj);
-            }
-            all_tables.push({ name: row["name"], schema: schemaArr });
+            const schema = getSchemaFromSql(queryStr); 
+            console.log(`Got schema: "${JSON.stringify(schema)}..."`);
+            all_tables.push({ name: row["name"], schema: schema });
         }
         
         // Render db view, given array of table metas
         function renderRows(err, rows) {
+            console.log(`Trying for "${JSON.stringify(all_tables)}..."`);
             res.render('db', { title: 'DB Tables Overview', all_tables: all_tables });
         }
         // Query db for rows, prepare array and render
         db.each("SELECT name, sql FROM sqlite_master WHERE type='table'",
-            processRows, renderRows);
-
+            processRow, renderRows);
     });
 
     // GET single table view
     app.get('/table/:name/', function(req, res, next) {
-        const tblname = req.params.name;
-        var tblsql = null;
+        const tblName = req.params.name;
+        var tblSchema = null;
         // Render table view, given array of rows
         function renderValidRows(err, rows) {
             res.render('table', {
-                title: 'DB Table · ' + tblname,
+                title: 'DB Table · ' + tblName,
                 all_rows: rows,
-                name: tblname 
+                name: tblName,
+                schema: tblSchema
             });
         }
         // Render view depending on validity of given sqlite_master row
@@ -64,8 +72,8 @@ function setupRouting(app, db) {
             console.log(`Trying for "${JSON.stringify(row)}..."`);
             if(row != undefined) {
                 // a table with given name exists
-                tblsql = row["sql"];
-                db.all("SELECT * FROM " + tblname,
+                tblSchema = getSchemaFromSql(row["sql"]);
+                db.all("SELECT * FROM " + tblName,
                 renderValidRows);
             } else {
                 // a table with given name does not exist
@@ -74,7 +82,7 @@ function setupRouting(app, db) {
         }
         // Query db for rows and render
         db.get(
-            `SELECT name, sql FROM sqlite_master WHERE type='table' AND name='${tblname}';`,
+            `SELECT name, sql FROM sqlite_master WHERE type='table' AND name='${tblName}';`,
             renderAny
         );
     });
